0b5e0b4d7fc68834f444c2d19c7c3c63e3c4b3a0
JeffreyRiley <57199670+JeffreyRiley@users.noreply.github.com>
2020-05-29 12:07:19 -0700

    small bugfixes & shutter button css

--- Hentai University PTR/scripts/characters/demon.js
+++ Hentai University PTR/scripts/characters/demon.js
@@ -539,6 +539,7 @@ function writeEvent(name) { //Plays the actual event.
 
 var phoneArray = [//Lists the potential text events the player can receive at the start of the day, depending on their trust.
 	//{index: "demonNewReward", trust: 102,},
+	{index: "demonReward", requirements: "?flag demon aqua; ?flag demon forest;"},
 ]
 
 function writePhoneEvent(name) { //Plays the relevant phone event
